f638076e48f2385d8923669369fae9fb871d4573
okothhillary <hillarywokoth@gmail.com>
2025-09-24 16:41:05 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using ConnectFour;
+
+using ConnectFour.Components;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSingleton<GameState>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios.
+    app.UseHsts();
+}
+
+// Show a custom error page for status codes (e.g., 404).
+app.UseStatusCodePagesWithReExecute("/Error", "?code={0}");
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
